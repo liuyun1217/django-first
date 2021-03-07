50c162269495ca783523216ebaa7eecd94fa67c0
liuyun_ucas@163.com <MCefkNVe96k6TU>
2021-03-07 22:49:54 +0800

    使用unittest来生成测试类。

--- mysite/function_tests.py
+++ mysite/function_tests.py
@@ -8,7 +8,19 @@
 """
 
 from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
