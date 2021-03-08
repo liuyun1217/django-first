7ba62bb0b8c300a60fa53095ece4bd35a5ca1d65
liuyun_ucas@163.com <MCefkNVe96k6TU>
2021-03-09 00:11:47 +0800

    解决一个POST个POST.get的差异问题。

--- mysite/function_tests.py
+++ mysite/function_tests.py
@@ -36,9 +36,12 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项中显示了"1. Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(30)
         table = self.browser.find_element_by_id('id_list_table')
+        # table.switch_to_window(table.window_handles[1])
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == "1. Buy peacock feathers" for row in rows ),"New to-do item did not appear in table")
+        self.assertIn("1. Buy peacock feathers",[row.text for row in rows] )
 
         # 页面又显示了一个文本框，可以输入其他待办事项
         # 输入"Use peacock feathers to make a fly"
